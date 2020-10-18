5904b0a99c111b71f76a4be5fe24f9944f33fee4
stevenclarke94 <stevenjsclarke@yahoo.co.uk>
2020-10-18 20:07:41 +0100

    Updates

---
+++ precache-manifest.47e69278eac5229240c6483ce5d409d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4606320317a41fca5670522f43bf03",
+    "url": "/reactTodoList/index.html"
+  },
+  {
+    "revision": "1d275fad41d5a3723fa2",
+    "url": "/reactTodoList/static/css/main.407592ca.chunk.css"
+  },
+  {
+    "revision": "ac82b6525046c4cf7ee5",
+    "url": "/reactTodoList/static/js/2.312d7260.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactTodoList/static/js/2.312d7260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d275fad41d5a3723fa2",
+    "url": "/reactTodoList/static/js/main.bdae4cd0.chunk.js"
+  },
+  {
+    "revision": "aa60bad7faa5e6f8e32a",
+    "url": "/reactTodoList/static/js/runtime-main.b6e95600.js"
+  },
+  {
+    "revision": "065579cff00549510f3eb1e2018512b9",
+    "url": "/reactTodoList/static/media/backgroundnew.065579cf.jpg"
+  }
+]);
